a352f61ac4f678d5acc12b83177f68f16dd5061b
Evan Placenis <placenie@mcmaster.ca>
2023-06-27 13:39:59 -0400

    fixed imports

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 import Setup from './components/Setup';
-import Objects from './components/Objects';
 
 function App() {
   return (
     <div>
       <Setup/>
-      {/* <Objects/>*/}
-
     </div>
     
   );
